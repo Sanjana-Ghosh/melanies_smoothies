0b3ad5030bb489db450814310aaa201fb510cbcb
Sanjana Ghosh <64126977+Sanjana-Ghosh@users.noreply.github.com>
2025-10-28 14:47:54 +0530

    Update streamlit_app.py
    removed get_active_session

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -16,7 +17,7 @@ session=cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your smoothie will be:", name_on_order)
 
-session = get_active_session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
